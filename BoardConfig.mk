bb764e4b4e92be847777cf623acf965aa9d5ba5c
Ubuntu <ubuntu@box1074.localdomain>
2017-04-28 09:12:46 +0000

    Initial X553

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,42 +1,98 @@
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME :=
-TARGET_NO_BOOTLOADER := true
+#
+# Copyright (C) 2016 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Platform
-TARGET_BOARD_PLATFORM :=
-TARGET_BOARD_PLATFORM_GPU :=
+LOCAL_PATH := device/Infinix/X553
+
 
 # Architecture
-TARGET_ARCH :=
-TARGET_ARCH_VARIANT :=
-TARGET_CPU_ABI :=
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT :=
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
 
-# For arm64
-TARGET_2ND_ARCH :=
-TARGET_2ND_ARCH_VARIANT :=
-TARGET_2ND_CPU_ABI :=
-TARGET_2ND_CPU_ABI2 :=
-TARGET_2ND_CPU_VARIANT :=
+TARGET_BOARD_SUFFIX := _64
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+ # actually a53, but the build is broken in omni
+TARGET_2ND_CPU_VARIANT := cortex-a15
+
+TARGET_USES_64_BIT_BINDER := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
+
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_LDPRELOAD := libxlog.so
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# BOOTLOADER
+TARGET_BOOTLOADER_BOARD_NAME := msm8916
 
 # Kernel
-BOARD_KERNEL_CMDLINE :=
-BOARD_KERNEL_BASE :=
-BOARD_KERNEL_PAGESIZE :=
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00000000 --ramdisk_offset 0x00000000 --dt device/vendor/codename/dt.img --tags_offset 0x00000000
-BOARD_KERNEL_SEPARATED_DT :=
-TARGET_PREBUILT_KERNEL := device/vendor/codename/kernel
+BOARD_KERNEL_CMDLINE += \
+	androidboot.hardware=qcom \
+	msm_rtb.filter=0x237 \
+	ehci-hcd.park=3 \
+	androidboot.bootdevice=7824900.sdhci \
+	lpm_levels.sleep_disabled=1 \
+	earlyprintk \ 
+	bootopt=64S3,32S1,32S1 \
+	androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS  := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board X553-W82918A1-L-160913V4
+TARGET_PREBUILT_KERNEL := device/Infinix/d5110/prebuilt/kernel
 
-# Partition info
+# TARGET IMAGES
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_PLATFORM_DEVICE_BASE :=
-BOARD_BOOTIMAGE_PARTITION_SIZE :=
-BOARD_RECOVERYIMAGE_PARTITION_SIZE :=
-BOARD_SYSTEMIMAGE_PARTITION_SIZE :=
-BOARD_USERDATAIMAGE_PARTITION_SIZE :=
-BOARD_FLASH_BLOCK_SIZE :=
 
-# TWRP
-TW_THEME :=
+# RECOVERY
+TARGET_RECOVERY_FSTAB := device/Infinix/X553/recovery/root/twrp.fstab
 
+# PARTTIONS
+# We need the partitions size in decimal
+# Use cat proc/partitions via adb, then block size * 1024
+# This is the partitions of Infinix ROM, system size is bigger than Android One ROM
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1887436800
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12949896192
+# For the following line, do KERNEL_PAGE_SIZE * 64 (Most of time it is 2048 * 64)
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# TWRP stuff
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_FB2PNG := true
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TW_REBOOT_BOOTLOADER := true
+TW_REBOOT_RECOVERY := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_EXCLUDE_SUPERSU := true
+TW_USE_TOOLBOX := true
